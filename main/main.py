a466cc4ea90994f2411ac7364089b141d1faa622
windfallw <51202308+windfallw@users.noreply.github.com>
2020-12-16 18:07:00 +0800

    update...

--- main/main.py
+++ main/main.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget, QFileDialog
-from PyQt5.QtCore import QTimer, QThread
+from PyQt5.QtCore import QTimer, QThread, pyqtSignal
 from PyQt5.QtGui import QIcon, QTextCursor
-from src.SLmge import *
 from rfid import RFID_YES, time
+from src.SLmge import *
+from sql import *
 import sys
 
 
@@ -10,26 +11,26 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.work = WorkThread()
 
         self.refreshPORTList()
-        self.port.currentIndexChanged.connect(self.changeSER)
-
         self.refreshBDList()
+
+        # self.work.trigger125.connect()
+        # self.work.trigger14443.connect()
+
+        self.port.currentIndexChanged.connect(self.changeSER)
         self.baud.currentIndexChanged.connect(self.changeBR)
-
         self.openSer.clicked.connect(self.openSER)
-
-        self.read125k.setAutoRepeat(False)
         self.read125k.clicked.connect(self.find125k)
-
-        self.read14443.setAutoRepeat(False)
         self.read14443.clicked.connect(self.find14443)
 
     def refreshBDList(self):
-        self.baud.clear()
-        self.baud.addItems(device.bd_list)
-        self.baud.setCurrentIndex(2)
-        device.ser.baudrate = int(self.baud.currentText())
+        if not device.ser.isOpen():
+            self.baud.clear()
+            self.baud.addItems(device.bd_list)
+            self.baud.setCurrentIndex(2)
+            device.ser.baudrate = int(self.baud.currentText())
 
     def refreshPORTList(self):
         self.port.clear()
@@ -47,7 +48,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def openSER(self):
         if device.open():
             self.openSer.setEnabled(False)
-            work.start()
+            self.work.start()
         else:
             QMessageBox.critical(self, '错误', '打开串口失败')
         # self.openSer.setCheckable(True)
@@ -65,8 +66,20 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         else:
             QMessageBox.information(self, '提示', '请先打开串口')
 
+    def throwErr(self, func, err=None):
+        try:
+            func()
+        except Exception as e:
+            if err:
+                QMessageBox.critical(self, '错误', err)
+            else:
+                QMessageBox.critical(self, '错误', str(e))
+
 
 class WorkThread(QThread):
+    trigger125 = pyqtSignal(bytes)
+    trigger14443 = pyqtSignal(bytes)
+
     def __init__(self, parent=None):
         super(WorkThread, self).__init__(parent)
         self.working = True
@@ -80,19 +93,19 @@ class WorkThread(QThread):
         while True:
             data = device.ser.readall()
             if data != b'':
-                content = data.decode("utf-8", "replace")
+                content = data.decode("utf-8", "replace")  # ignore or replace
                 win.serialPrint.moveCursor(QTextCursor.End)
                 win.serialPrint.insertPlainText(content)
+                self.trigger125.emit(data)
+                self.trigger14443.emit(data)
                 # print(content, end='')
-                # decode ignore or replace
             time.sleep(0.005)
 
 
 device = RFID_YES()
-work = WorkThread()
 
 app = QApplication(sys.argv)
-app.setWindowIcon(QIcon("./src/icon.svg"))
+app.setWindowIcon(QIcon("icon.svg"))
 
 win = MyMainWindow()
 win.show()
--- main/sql.py
+++ main/sql.py
@@ -3,24 +3,40 @@ import sqlite3
 conn = sqlite3.connect('rfid.db')
 
 print("Opened database successfully")
-c = conn.cursor()
-c.execute('''CREATE TABLE if not exists student
+sqlite = conn.cursor()
+sqlite.execute('''CREATE TABLE if not exists student
        (id    text      PRIMARY KEY NOT NULL,
        name   text(10)  NOT NULL,
        number char(10)  NOT NULL,
        sex    text(10),
        major  text(10));''')
 conn.commit()
-conn.close()
 
 
-def insertSQL():
-    ...
+def getSQL():
+    result = sqlite.execute('''select * from student;''')
+    print(result.fetchall())
+    return result
 
 
-def removeSQL():
-    ...
+def insertSQL(ID, name, number, sex='null', major='null'):
+    query = '''insert into student(id,name,number,sex,major)
+    values ({0},{1},{2},{3},{4});'''.format(ID, name, number, sex, major)
+    print(query)
+    sqlite.execute(query)
+    conn.commit()
 
 
-def updateSQL():
-    ...
+def updateSQL(ID, name, number, sex='null', major='null'):
+    sqlite.execute('''update student set  name={0},number={1},sex={2},major={3}
+    where id={4};'''.format(name, number, sex, major, ID))
+    conn.commit()
+
+
+def removeSQL(ID):
+    sqlite.execute('''delete from student
+    where id={0};'''.format(ID))
+    conn.commit()
+
+
+getSQL()
