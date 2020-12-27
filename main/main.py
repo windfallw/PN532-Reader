cded83417a156d9546734a0ca6caf5dc76a82da8
windfallw <51202308+windfallw@users.noreply.github.com>
2020-12-28 00:16:01 +0800

    pn532 get card id success

--- main/main.py
+++ main/main.py
@@ -67,6 +67,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     def printByte(self, data):
         self.serialPrint.moveCursor(QTextCursor.End)
+        # self.serialPrint.append(str(data))
         self.serialPrint.append(data.hex(' '))
 
     def printStr(self, data):
@@ -93,8 +94,11 @@ class WorkThread(QThread):
             self.trigger_str.emit('wake up PN532 successfully .')
         while True:
             data = device.ser.readall()
-            if data != b'':
-                self.trigger_byte.emit(data)
+            if data.startswith(b'\x00\x00\xff\x00\xff\x00') and len(data) > 6:
+                res = data.replace(b'\x00\x00\xff\x00\xff\x00', b'')
+                self.trigger_byte.emit(res)
+                if res.startswith(b'\x00\x00\xff\x0c\xf4\xd5K\x01\x01\x00\x04\x08\x04') and res.endswith(b'\x00'):
+                    self.trigger_byte.emit(res[13:-2])
 
             time.sleep(0.005)
 
